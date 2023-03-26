e190e45c5820c81cae8abae62f835dc5405f0f9f
inksoot <abc9326@nyu.edu>
2023-03-26 16:06:41 -0400

    boundaries (need to fix camera)

--- Assets/Scripts/dialogueTrigger.cs
+++ Assets/Scripts/dialogueTrigger.cs
@@ -6,8 +6,18 @@ public class dialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(gameObject.tag == "Player")
+        {
+            Debug.Log("enemy contact with player");
+            FindObjectOfType<dialogueManager>().StartDialogue(dialogue);
+        }
+    }
+
     public void TriggerDialogue()
     {
+        
         FindObjectOfType<dialogueManager>().StartDialogue(dialogue);
     }
 }
